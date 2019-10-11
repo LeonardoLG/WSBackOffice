c2762bec18c42a49d98e2479c814ecf478b89768
LeonardoLG <leonardoleongutierrez27@gmail.com>
2019-10-11 02:33:19 -0500

    Avance 11.10.19

--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -7,8 +7,8 @@ function initMap() {
 
     var ubicaciones = [
         {"title": "Ganado 1", "lat": -7.1727793, "lng": -78.47969, "zIndex": 4},
-        {"title": "Ganado 2", "lat": -7.1729013, "lng": -78.47932, "zIndex": 5}, 
-        {"title": "Ganado 3", "lat": -7.1723533, "lng": -78.479905, "zIndex": 3}, 
+        {"title": "Ganado 2", "lat": -7.1729013, "lng": -78.47932, "zIndex": 5},
+        {"title": "Ganado 3", "lat": -7.1723533, "lng": -78.479905, "zIndex": 3},
         {"title": "Ganado 4", "lat": -7.1727203, "lng": -78.478419, "zIndex": 2}];
 //    var ubicaciones = [
 //        ['Ganado 1', -7.1727793, -78.47969, 4],
@@ -30,7 +30,7 @@ function setUbicacion(map, ubicaciones) {
         // The anchor for this image is the base of the flagpole at (0, 32).
         anchor: new google.maps.Point(0, 32)
     };
-    
+
     var shape = {
         coords: [1, 1, 1, 20, 18, 20, 18, 1],
         type: 'poly'
@@ -48,9 +48,14 @@ function setUbicacion(map, ubicaciones) {
     }
 }
 
+function actualizar() {
+    actualizarUbicaciones();
+    actualizarAlertas();
+}
+
 function actualizarUbicaciones() {
     $.ajax({
-        url: "http://192.168.1.40:8090/WSCore/resources/service"
+        url: "http://192.168.1.40:8090/WSCore/resources/service/ubicaciones"
     }).then(function (data) {
         var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 18,
@@ -59,4 +64,24 @@ function actualizarUbicaciones() {
         });
         setUbicacion(map, data);
     });
+}
+
+function actualizarAlertas() {
+    var contenido = document.querySelector('#tab_alerta');
+    $.ajax({
+        url: "http://192.168.1.40:8090/WSCore/resources/service/alertas"
+    }).then(function (data) {
+        llenarTabla(data, contenido);
+    });
+}
+
+function llenarTabla(data, contenido) {
+    contenido.innerHTML = '';
+    for (let item of data) {
+        contenido.innerHTML += `
+        <tr>
+            <th>${item.nombreGanado}</th>                       
+            <th>${item.fechaAlerta}</th>                       
+        </tr>`
+    }
 }
